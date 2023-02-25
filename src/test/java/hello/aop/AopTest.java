c0aec7d9cb27ed1a84e74aebbd8978215f91633e
jiwon615 <seg615@naver.com>
2023-02-25 10:00:22 +0900

    comment: 주석 추가

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -3,6 +3,7 @@ package hello.aop;
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +22,10 @@ public class AopTest {
     OrderRepository orderRepository;
 
     @Test
+    @DisplayName("AOP 관련 코드 작성하지 않았으므로, 프록시도 적용되지 않고 결과도 false 반환")
     void aopInfo() {
-        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
-        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService)); // false
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository)); // false
     }
 
     @Test
