b624e3121a3c2f408a247568fe3ea2a9af1f616d
jiwon615 <seg615@naver.com>
2023-02-26 14:58:03 +0900

    comment: 주석

--- src/main/java/hello/aop/exam/ExamService.java
+++ src/main/java/hello/aop/exam/ExamService.java
@@ -1,6 +1,5 @@
 package hello.aop.exam;
 
-import hello.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
--- src/main/java/hello/aop/exam/annotation/Trace.java
+++ src/main/java/hello/aop/exam/annotation/Trace.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+// 로그출력 애노테이션
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Trace {
